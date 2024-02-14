55533e9dcca3c1b9116c8556d3501a3a6a8692dd
nodirdev984 <negmatovn984@gmail.com>
2024-02-14 19:09:26 +0500

    essay

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="max-w-[90%] md:max-w-[70%] mt-4 md:mt-6 mx-auto text-[16px] md:text-[22px] font-semibold flex flex-col gap-4">
+        <p>
+          The map provides a layout of the public park when it first opened in
+          1920 and the same park today.
+        </p>
+        <p>
+          Overall, Grange Park, especially to relax from the clear landscape,
+          has many seats for sitting and musical areas for singing songs in the
+          past and today.
+        </p>
+        <p>
+          In 1920, this park included many seats and rose gardens around the
+          ground of the park, so there was a lot of disorder in the park. And
+          this park had two main things. There was a small stage for musicians
+          and a beautiful fountain. You could enter this park from two main
+          entrances; their names were Arnold Avenue and Eldon Street.
+        </p>
+        <p>
+          Today, this park is better than before and very modern. You can see
+          only two rose gardens, which have been added to this park. There is a
+          cafe, a children’s play area, and a big water feature in Grange Park.
+          And the stage for musicians has become a modern amphitheater for
+          concerts, but you cannot see a fountain. This place has a rose garden
+          and is surrounded by seats. The two entrances do not change, but an
+          underground car park entrance is pushed to Grange Park.
+        </p>
+      </div>
+      <div className="text-xl md:text-2xl font-bold  mx-[15%] mt-6">
+        {" "}
+        Negmatov Nodirbek's essay for Sitora teacher (190 Words)
+      </div>
+    </>
+  );
+}
+
+export default App;
